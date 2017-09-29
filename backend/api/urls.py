eee86c2e41cb1e772463b539bea7de09c9de3b0b
Sergio Garcia <srgarcia91@gmail.com>
2017-09-29 19:47:09 +0200

    Including todos app on api urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.authtoken import views as drf_views
 
 urlpatterns = [
   url(r'^auth$', drf_views.obtain_auth_token, name='auth'),
+  url(r'^', include('todos.urls', namespace='todos'))
 ]
