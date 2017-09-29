11eafcf8ac04e4393f5b900a6fe934c495d6f189
Sergio Garcia <srgarcia91@gmail.com>
2017-09-29 20:39:17 +0200

    Added users API endpoint

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -4,4 +4,6 @@ from todos import views
 urlpatterns = [
     url(r'^todos/$', views.TodoList.as_view()),
     url(r'^todos/(?P<pk>[0-9]+)/$', views.TodoDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
