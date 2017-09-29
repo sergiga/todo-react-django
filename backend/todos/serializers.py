281c883a10af3c93706b3d2d62ba6b49790e9c9b
Sergio Garcia <srgarcia91@gmail.com>
2017-09-29 20:28:30 +0200

    Created User serializer

--- backend/todos/serializers.py
+++ backend/todos/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from todos.models import Todo
+from django.contrib.auth.models import User
 
 class TodoSerializer(serializers.ModelSerializer):
   class Meta:
     model = Todo
     fields = ('id', 'title', 'content')
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ('id', 'username', 'todos')
