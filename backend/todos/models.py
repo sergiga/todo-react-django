52cdf5fd12d9601b1727168f89b8e848d158d5f6
Sergio Garcia <srgarcia91@gmail.com>
2017-09-29 19:17:32 +0200

    Added todo model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(model.Model):
+  created = models.DateTimeField(auto_now_add=True)
+  title = models.CharField(max_length=100, blank=True, default='')
+  content = models.CharField(max_length=140, blank=True, default='')
+
+  class Meta:
+    ordering = ('created',)
