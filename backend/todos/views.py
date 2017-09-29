b171dac036edc316a7d9381bf03cad5c660be056
Sergio Garcia <srgarcia91@gmail.com>
2017-09-29 20:49:58 +0200

    Adding read only permission on todos if not Auth

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -5,8 +5,11 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
 
 class TodoList(APIView):
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
   def get(self, request, format=None):
     todos = Todo.objects.all()
     serializer = TodoSerializer(todos, many=True)
@@ -20,6 +23,8 @@ class TodoList(APIView):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TodoDetail(APIView):
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+  
   def get_object(self, pk):
     try:
       return Todo.objects.get(pk=pk)
