6d5dc19d096b20dd9a86296b9f14e79528cf0a57
Sergio Garcia <srgarcia91@gmail.com>
2017-09-29 20:41:22 +0200

    Added the user views

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,5 +1,6 @@
 from todos.models import Todo
-from todos.serializers import TodoSerializer
+from django.contrib.auth.models import User
+from todos.serializers import TodoSerializer, UserSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -41,4 +42,45 @@ class TodoDetail(APIView):
   def delete(self, request, pk, format=None):
     todo = self.get_object(pk)
     todo.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+  def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
+
+class UserList(APIView):
+  def get(self, request, format=None):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+  def post(self, request, format=None):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetail(APIView):
+  def get_object(self, pk):
+    try:
+      return User.objects.get(pk=pk)
+    except User.DoesNotExist:
+      raise Http404
+
+  def get(self, request, pk, format=None):
+    user = self.get_object(pk)
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+  def put(self, request, pk, format=None):
+    todo = self.get_object(pk)
+    serializer = UserSerializer(user, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    user = self.get_object(pk)
+    user.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
