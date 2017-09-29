2f795af76ccc90d68a967b222a61d8265803e57c
Sergio Garcia <srgarcia91@gmail.com>
2017-09-29 19:21:32 +0200

    Migrating the todo model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Todo(model.Model):
+class Todo(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   title = models.CharField(max_length=100, blank=True, default='')
   content = models.CharField(max_length=140, blank=True, default='')
