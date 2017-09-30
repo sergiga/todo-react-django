45482bc7fba2ea93bf93408c5c1ad1b65cd8c785
Sergio Garcia <srgarcia91@gmail.com>
2017-09-30 18:44:10 +0200

    Added the API call function

--- frontend/src/middleware/api.js
+++ frontend/src/middleware/api.js
@@ -1,4 +1,21 @@
 import { normalize, schema } from 'normalizr';
+import axios from 'axios';
+import omit from 'lodash/omit';
+import merge from 'lodash/merge';
+
+// API call
+const API_ROOT = 'http://localhost/8000/';
+const callApi = (endpoint, query, schema) => {
+  const fullUrl = (endpoint.indexOf(API_ROOT) === -1) 
+    ? API_ROOT + endpoint 
+    : endpoint;
+
+  return axios.get(fullUrl, { params: query }, {
+    validateStatus: status => { return status >= 200 && status <= 500 }
+  }).then(({ data }) => {
+    return merge({}, normalize(data, schema));
+  }); 
+}
 
 // Schemas
 const todo = new schema.Entity('todos', {}, {
