abdfbdb289e53e41cdab90aa4e9757639cb60f61
Sergio Garcia <srgarcia91@gmail.com>
2017-09-30 18:45:51 +0200

    Redux middleware function

--- frontend/src/middleware/api.js
+++ frontend/src/middleware/api.js
@@ -26,3 +26,53 @@ export const Schemas = {
   TODO: todo,
   TODO_ARRAY: [ todo ]
 }
+
+// Redux middleware
+export const API_MIDDLEWARE = 'API_MIDDLEWARE';
+
+export default store => next => action => {
+  const apiCall = action[API_MIDDLEWARE];
+
+  if(!apiCall) {
+    return next(action);
+  }
+
+  const { endpoint, query, types, schema } = apiCall;
+
+  if(typeof endpoint !== 'string') {
+    throw new Error('Expecting the endpoint to be a string');
+  }
+  if(typeof query !== 'object') {
+    throw new Error('Expecting the endpoint to be an object');
+  }
+  if(!Array.isArray(types)) {
+    throw new Error('Expecting an array of types.');
+  }
+  if(types.length !== 3) {
+    throw new Error('Expecting three action types.')
+  }
+  if(!types.every(type => typeof type === 'string')) {
+    throw new Error('Expecting three action types to be strings.');
+  }
+
+  const actionWith = data => {
+    const finalAction = merge({}, action, data);
+    delete finalAction[API_MIDDLEWARE];
+    return finalAction;
+  }
+
+  const [ requestType, successType, failureType] = types;
+
+  next(actionWith({ type: requestType }));
+
+  return callApi(endpoint, query, schema).then(
+    response => next(actionWith({
+      response,
+      type: successType
+    })),
+    error => next(actionWith({
+      type: failureType,
+      error: error.message || 'Something bad happened'
+    }))
+  );
+}
