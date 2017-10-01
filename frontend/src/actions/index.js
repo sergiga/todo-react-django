22611e4b51a1afcf80b262920838bcfeee2b9dcf
Sergio Garcia <srgarcia91@gmail.com>
2017-10-01 21:48:05 +0200

    CRUD action methods for todos

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -17,6 +17,48 @@ export const loadTodos = () => (dispatch) => {
   return dispatch(fetchTodos())
 }
 
+export const CREATE_TODO_REQUEST = 'CREATE_TODO_REQUEST'; 
+export const CREATE_TODO_SUCCESS = 'CREATE_TODO_SUCCESS'; 
+export const CREATE_TODO_FAILURE = 'CREATE_TODO_FAILURE';
+
+export const createTodo = (todo) => (dispatch) => {
+  return dispatch({
+    [API_MIDDLEWARE]: {
+      method: Methods.POST,
+      types: [ CREATE_TODO_REQUEST, CREATE_TODO_SUCCESS, CREATE_TODO_FAILURE ],
+      endpoint: 'todos/'
+    }
+  });
+}
+
+export const UPDATE_TODO_REQUEST = 'UPDATE_TODO_REQUEST'; 
+export const UPDATE_TODO_SUCCESS = 'UPDATE_TODO_SUCCESS'; 
+export const UPDATE_TODO_FAILURE = 'UPDATE_TODO_FAILURE';
+
+export const updateTodo = (todoID) => (dispatch) => {
+  return dispatch({
+    [API_MIDDLEWARE]: {
+      method: Methods.PUT,
+      types: [ UPDATE_TODO_REQUEST, UPDATE_TODO_SUCCESS, UPDATE_TODO_FAILURE ],
+      endpoint: `todos/${todoID}/`
+    }
+  });
+}
+
+export const DELETE_TODO_REQUEST = 'DELETE_TODO_REQUEST'; 
+export const DELETE_TODO_SUCCESS = 'DELETE_TODO_SUCCESS'; 
+export const DELETE_TODO_FAILURE = 'DELETE_TODO_FAILURE';
+
+export const deleteTodo = (todoID) => (dispatch) => {
+  return dispatch({
+    [API_MIDDLEWARE]: {
+      method: Methods.DELETE,
+      types: [ DELETE_TODO_REQUEST, DELETE_TODO_SUCCESS, DELETE_TODO_FAILURE ],
+      endpoint: `todos/${todoID}/`
+    }
+  });
+}
+
 export const SHOW_VISIBILITY_FILTER = 'SHOW_VISIBILITY_FILTER';
 
 export const setVisibilityFilter = (filter) => (dispatch, getState) => {
