d3002801cd25daaa250863c5032d14d36ac075d3
Sergio Garcia <srgarcia91@gmail.com>
2017-10-01 21:00:21 +0200

    Added completed field to todos

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Todo(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   title = models.CharField(max_length=100, blank=True, default='')
-  content = models.CharField(max_length=140, blank=True, default='')
+  completed = models.BooleanField(default=False)
   owner = models.ForeignKey('auth.User', related_name='todos', on_delete=models.CASCADE, default=1)
 
   class Meta:
