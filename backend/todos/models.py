31710097de0aec809c9d6e34dc3df74d334f0629
Sergio Garcia <srgarcia91@gmail.com>
2017-10-02 18:12:12 +0200

    Changed the todo model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Todo(models.Model):
   created = models.DateTimeField(auto_now_add=True)
-  title = models.CharField(max_length=100, blank=True, default='')
+  title = models.CharField(max_length=100, blank=False)
   completed = models.BooleanField(default=False)
   owner = models.ForeignKey('auth.User', related_name='todos', on_delete=models.CASCADE, default=1)
 
