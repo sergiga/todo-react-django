93ee0b43327cdbe00ab9b734a194a5f03278df36
Sergio Garcia <srgarcia91@gmail.com>
2017-10-02 19:10:46 +0200

    Reducer for POST todo success

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -3,10 +3,17 @@ import merge from 'lodash/merge';
 import { combineReducers } from 'redux';
 
 const todos = (state = {}, action) => {
-  if (action.response && action.response.entities && action.response.entities.todos) {
-    return merge({}, state, action.response.entities.todos);
+  switch(action.type) {
+    case ActionTypes.TODOS_SUCCESS:
+    case ActionTypes.CREATE_TODO_SUCCESS:
+      if (action.response && action.response.entities && action.response.entities.todos) {
+        return merge({}, state, action.response.entities.todos);
+      }
+      return state;
+    case ActionTypes.DELETE_TODO_SUCCESS:
+    default:
+      return state;
   }
-  return state;
 }
 
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
