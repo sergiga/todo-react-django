39be6acf2224af36014ddc5395a46cf71376a080
Sergio Garcia <srgarcia91@gmail.com>
2017-10-03 21:30:14 +0200

    Created visibility filter component

--- frontend/src/components/VisibilityFilter.js
+++ frontend/src/components/VisibilityFilter.js
@@ -6,7 +6,26 @@ class VisibilityFilter extends Component {
     setVisibilityFilter: PropTypes.func.isRequired
   }
 
-  render() { return null }
+  constructor(props) {
+    super(props);
+
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+  }
+
+  handleFilterClick(filter) {
+    this.props.setVisibilityFilter(filter);
+  }
+
+  render() {
+    return (
+      <div>
+        <span>Filter by:</span>
+        <button onClick={() => this.handleFilterClick('SHOW_ALL')}>ALL</button>
+        <button onClick={() => this.handleFilterClick('SHOW_ACTIVE')}>ACTIVE</button>
+        <button onClick={() => this.handleFilterClick('SHOW_COMPLETED')}>COMPLETED</button>
+      </div>
+    );
+  }
 }
 
 export default VisibilityFilter;
