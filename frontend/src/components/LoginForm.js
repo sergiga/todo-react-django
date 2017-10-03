68357cb066c0db5f71f53aba798c85ed759b3f89
Sergio Garcia <srgarcia91@gmail.com>
2017-10-03 20:31:50 +0200

    Login form component created

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -6,7 +6,54 @@ class LoginForm extends Component {
     login: PropTypes.func.isRequired
   }
 
-  render() { return null }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { username, password } = this.state;
+    this.props.login({ username, password});
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value =  target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    const { username, password } = this.state;
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          name='username'
+          type='text'
+          value={username}
+          placeholder='username'
+          onChange={this.handleChange} />
+        <input 
+          name='password'
+          type='text'
+          value={password}
+          placeholder='password'
+          onChange={this.handleChange} />
+        <input type='submit' value='LOGIN'/>
+      </form>
+    ); 
+  }
 }
 
 export default LoginForm;
