020c05c791a3dc0712e16d4b29c39f198362cada
Sergio Garcia <srgarcia91@gmail.com>
2017-10-03 21:14:59 +0200

    Created Todo component

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -3,9 +3,29 @@ import PropTypes from 'prop-types';
 
 class Todo extends Component {
   static propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+    owner: PropTypes.string.isRequired,
+    updateTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired
   }
 
-  render() { return null }
+  render() {
+    const { id, title, completed, updateTodo, deleteTodo } = this.props;
+
+    return (
+      <div>
+        <input
+          name='completed'
+          type='checkbox'
+          checked={completed}
+          onChange={(e) => updateTodo(id, {completed: e.target.checked})} />
+        <span>{title}</span>
+        <button onClick={() => deleteTodo(id)}>DELETE</button>
+      </div>
+    );
+  }
 }
 
 export default Todo;
