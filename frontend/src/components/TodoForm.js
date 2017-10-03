3176a6a150d6ee0bb1064b3ad8251ce90b40129c
Sergio Garcia <srgarcia91@gmail.com>
2017-10-03 20:45:43 +0200

    Created the todo form

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -3,10 +3,53 @@ import PropTypes from 'prop-types';
 
 class TodoForm extends Component {
   static propTypes = {
-    submitForm: PropTypes.func.isRequired
+    submitTodo: PropTypes.func.isRequired
   }
-  
-  render() { return null }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { title } = this.state;
+    this.props.submitTodo({ title });
+    this.setState({
+      title: ''
+    })
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value =  target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    const { title } = this.state;
+
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          name='title'
+          type='text'
+          value={title}
+          placeholder='What has to be done?'
+          onChange={this.handleChange} />
+      </form>
+    ); 
+  }
 }
 
 export default TodoForm;
