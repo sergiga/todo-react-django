e54ccdcba09a0fe7047ff73c2ae9012d9c32c8fa
Sergio Garcia <srgarcia91@gmail.com>
2017-10-03 20:55:12 +0200

    Created List component

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class List extends Component {
-  static propTypes = {
-    items: PropTypes.array.isRequired,
-    renderItem: PropTypes.func.isRequired
-  }
+  render() {
+    const { items, renderItem } = this.props;
 
-  render() { return null }
+    if(!items) { return null; }
+
+    return(
+      <div className='list'>
+        <div className='list-container'>
+          {items.map(item => <div key={item.id} className='list-item'>{renderItem(item)}</div>)}    
+        </div>
+      </div>
+    );
+  }
+}
+
+List.propTypes = {
+  items: PropTypes.array,
+  renderItem: PropTypes.func.isRequired,
 }
 
 export default List;
