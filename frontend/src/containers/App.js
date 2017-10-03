cef3d919f03cfbbf5a2fb23fc05f1b3f46715747
Sergio Garcia <srgarcia91@gmail.com>
2017-10-03 18:09:40 +0200

    Connecting the App component to the Redux store

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { 
+  loadTodos,
+  createTodo, 
+  updateTodo, 
+  deleteTodo, 
+  setVisibilityFilter 
+} from '../actions/index';
 
-const App = (props) => (
-  <div className='main-container'>
-    { props.children }
-  </div>
-)
+class App extends Component {
+  static propTypes = {
+    userTodos: PropTypes.array.isRequired,
+    loadTodos: PropTypes.func.isRequired,
+    createTodo: PropTypes.func.isRequired, 
+    updateTodo: PropTypes.func.isRequired, 
+    deleteTodo: PropTypes.func.isRequired, 
+    setVisibilityFilter: PropTypes.func.isRequired
+  }
 
-App.propTypes = {
-  children: PropTypes.node
+  render() {
+    return (
+      <div />
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => {
+  const {
+    entities: { todos },
+    visibilityFilter
+  } = state;
+  
+  const userTodos = Object.values(todos);
+
+  return {
+    userTodos, 
+    visibilityFilter
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  {
+    loadTodos,
+    createTodo, 
+    updateTodo, 
+    deleteTodo, 
+    setVisibilityFilter 
+  }
+)(App);
